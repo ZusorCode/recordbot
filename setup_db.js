72526c41960391ca42a4d3ce1c3dc4354aac8368
Tobias Messner <tobias.d.messner@gmail.com>
2020-01-09 17:58:31 +0000

    Fixed table create

--- setup_db.js
+++ setup_db.js
@@ -1,6 +1,6 @@
 const Database = require('better-sqlite3');
 const db = new Database('main.db');
-db.prepare("DROP TABLE users").run();
-db.prepare("DROP TABLE events").run();
+db.prepare("DROP TABLE IF EXISTS users").run();
+db.prepare("DROP TABLE IF EXISTS events").run();
 db.prepare("CREATE TABLE users (user_id INTEGER UNIQUE, username TEXT UNIQUE, token TEXT UNIQUE, enabled INTEGER, record_reset INTEGER, record_text TEXT, record_text_chat TEXT, custom_html TEXT, custom_css TEXT)").run();
 db.prepare("CREATE TABLE events (user_id INTEGER, type TEXT, time NUMERIC)").run();
